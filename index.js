29a179c0e2e7619995bf7823748ced5c337b1361
Michael <mikahado@gmail.com>
2022-08-14 23:41:44 -0400

    message

---
+++ index.js
@@ -0,0 +1,10 @@
+function introductionWithLanguage(name, language);
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}`);
+
+function introductionWithLanguageOptional(name, language = "JavaScript");
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}`);
+
+
+
+
+
