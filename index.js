302cb7449d739363e95994c9ce710b3a06e8a870
Michael <mikahado@gmail.com>
2022-08-14 23:44:15 -0400

    commit tree 10bb593ff47f3015b807778ad519cc52703187c1

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-function introductionWithLanguage(name, language);
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}`);
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
 
-function introductionWithLanguageOptional(name, language = "JavaScript");
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}`);
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
 
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
 
-
-
-
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
